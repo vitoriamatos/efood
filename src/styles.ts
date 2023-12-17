2109b417eec160e21f3d48c81261aa545f646589
vitoriamatos <vitoria.matos99@gmail.com>
2023-12-17 14:41:38 -0300

    ajustes

--- src/styles.ts
+++ src/styles.ts
@@ -1,16 +1,20 @@
-import { createGlobalStyle } from "styled-components";
-
-const GlobalCss = createGlobalStyle`
-  *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: Roboto, sans-serif;
-  }
-`
+import { createGlobalStyle } from 'styled-components'
 
 const Colors = {
   withe: '#FFF',
   ligthOrgange: '#FFF8F2',
   red: '#E66767'
 }
+
+export const GlobalCss = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+  }
+
+  body{
+    background-color: ${Colors.ligthOrgange}
+  }
+`
