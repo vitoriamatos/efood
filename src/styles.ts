e806ea5800f522fa179e3e98bfe59b467382071b
vitoriamatos <vitoria.matos99@gmail.com>
2023-12-17 14:34:59 -0300

    ajustes

---
+++ src/styles.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalCss = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+  }
+`
+
+const Colors = {
+  withe: '#FFF',
+  ligthOrgange: '#FFF8F2',
+  red: '#E66767'
+}
