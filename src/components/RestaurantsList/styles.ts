07de070982fb44f183edc46cfc322f4cfcd17be6
vitoriamatos <vitoria.matos99@gmail.com>
2024-01-23 16:14:22 -0300

    AJUSTES PRETTIER

--- src/components/RestaurantsList/styles.ts
+++ src/components/RestaurantsList/styles.ts
@@ -8,7 +8,7 @@ export const breakpoints = {
 export const Container = styled.section`
   padding: 32px 0;
 `
- 
+
 export const List = styled.ul`
   display: grid;
   grid-template-columns: 1fr 1fr;
